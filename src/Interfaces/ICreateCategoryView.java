ecf22ae515d0890f92e09cfc61e1104e9527c01a
Dang Tu <dtu161205@gmail.com>
2025-05-20 16:47:37 +0700

    Xóa file cũ và cập nhật code mới

--- src/Interfaces/ICreateCategoryView.java
+++
@@ -1,9 +0,0 @@
-package Interfaces;
-
-import java.awt.event.ActionListener;
-
-public interface ICreateCategoryView {
-	public String getCategoryName();
-
-	public void setTaoListener(ActionListener listener);
-}
